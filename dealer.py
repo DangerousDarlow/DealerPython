96e60825490639a9b046c32003d384cd8698b3eb
Nick Darlow <dangerous.darlow@gmail.com>
2019-12-25 18:21:43 +0000

    single random deal

--- dealer.py
+++ dealer.py
@@ -1,15 +1,35 @@
 import json
 import random
 
-def total_cards(cards):
+def get_count(card):
+    if 'count' in card:
+        return card['count']
+    else:
+        card['count'] = 1
+        return 1
+
+
+def get_total(cards):
     total = 0
     for card in cards:
-        if 'count' in card:
-            total += card['count']
-        else:
-            total += 1
+        total += get_count(card)
 
     return total
+
+
+def calculate_probabilities(cards):
+    total = get_total(cards)
+    for card in cards:
+        count = get_count(card)
+        card['probability'] = count / total
+
+
+def draw_card(cards, random):
+    cumulative_probability = 0
+    for card in cards:
+        cumulative_probability += card['probability']
+        if cumulative_probability >= random:
+            return card
 
 
 def random_values(n):
@@ -20,12 +40,29 @@ def random_values(n):
     return values
 
 
-cards = {}
-with open('cards.json') as cards_file:
-    cards = json.load(cards_file)
+if __name__ == "__main__":
+    cards = {}
+    with open('cards.json') as cards_file:
+        cards = json.load(cards_file)
 
-players = {}
-with open('players.json') as players_file:
-    players = json.load(players_file)
+    players = {}
+    with open('players.json') as players_file:
+        players = json.load(players_file)
 
-print(random_values(5))
+    randoms = random_values(len(players))
+
+    for index in range(len(players)):
+        print('index', index, 'random', randoms[index])
+
+        player = players[index]
+        calculate_probabilities(cards)
+        print(json.dumps(cards, indent=2))
+
+        card = draw_card(cards, randoms[index])
+        player['card'] = card
+        print(json.dumps(player, indent=2))
+
+        count = get_count(card)
+        card['count'] = count -1
+        print()
+        print()
